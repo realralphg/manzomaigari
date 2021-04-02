2f7e112766e2921b5d7a5db5efeab66845a4a993
Usman Murtala <uzlash16@gmail.com>
2021-04-02 15:33:51 +0100

    update 0.1 header

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,44 +1,163 @@
 <template>
   <q-layout view="lHh Lpr lFf">
-    <q-header elevated>
-      <q-toolbar>
-        <q-btn
-          flat
-          dense
-          round
-          icon="menu"
-          aria-label="Menu"
-          @click="leftDrawerOpen = !leftDrawerOpen"
-        />
-
-        <q-toolbar-title>
-          Quasar App
-        </q-toolbar-title>
-
-        <div>Quasar v{{ $q.version }}</div>
+    <!--Work on reveal and reveal offset-->
+    <q-header reveal :reveal-offset="500">
+      <q-toolbar class="text-white bg-grey-10">
+        <div class="my-font-greatvibes-regular text-h4 q-pa-sm">
+          Dr. Manzo Maigari
+        </div>
+        <q-space />
+        <q-tabs narrow-indicator dense active-color="pink">
+          <q-route-tab
+            label="Home"
+            sm
+            ripple
+            no-caps
+            class="gt-sm q-mx-sm my-font-montserrat-light"
+            to="/"
+            exact
+          />
+          <q-route-tab
+            label="About Me"
+            ripple
+            no-caps
+            class="gt-sm q-mx-sm my-font-montserrat-light"
+            to="/about"
+            exact
+          />
+          <q-route-tab
+            label="Blog"
+            ripple
+            no-caps
+            class="gt-sm q-mx-sm my-font-montserrat-light"
+            to="/membership"
+            exact
+          />
+          <q-route-tab
+            label="Media"
+            ripple
+            no-caps
+            class="gt-sm q-mx-sm my-font-montserrat-light"
+            to="/leadership"
+            exact
+          />
+          <q-route-tab
+            label="Contact Me"
+            ripple
+            no-caps
+            class="gt-sm q-mx-sm my-font-montserrat-light"
+            to="/contact"
+            exact
+          />
+        </q-tabs>
+        <div class="q-pa-sm lt-md">
+          <q-btn flat round dense icon="menu" class="q-mr-sm">
+            <q-menu>
+              <q-list>
+                <q-item clickable v-close-popup>
+                  <q-item-section>
+                    <q-item-label
+                      ><q-btn
+                        stretch
+                        flat
+                        label="Home"
+                        to="/"
+                        no-caps
+                        size="md"
+                        class="q-mx-sm my-font-montserrat-light"
+                    /></q-item-label>
+                  </q-item-section>
+                </q-item>
+                <q-item clickable v-close-popup>
+                  <q-item-section>
+                    <q-item-label
+                      ><q-btn
+                        stretch
+                        flat
+                        label="About Us"
+                        to="/about"
+                        no-caps
+                        size="md"
+                        class="q-mx-sm my-font-montserrat-light"
+                    /></q-item-label>
+                  </q-item-section>
+                </q-item>
+                <q-item clickable v-close-popup>
+                  <q-item-section>
+                    <q-item-label
+                      ><q-btn
+                        stretch
+                        flat
+                        label="Membership"
+                        to="/membership"
+                        no-caps
+                        size="md"
+                        class="q-mx-sm my-font-montserrat-light"
+                    /></q-item-label>
+                  </q-item-section>
+                </q-item>
+                <q-item clickable v-close-popup>
+                  <q-item-section>
+                    <q-item-label
+                      ><q-btn
+                        stretch
+                        flat
+                        label="Leadership"
+                        to="/leadership"
+                        no-caps
+                        size="md"
+                        class="q-mx-sm my-font-montserrat-light"
+                    /></q-item-label>
+                  </q-item-section>
+                </q-item>
+                <q-item clickable v-close-popup>
+                  <q-item-section>
+                    <q-item-label
+                      ><q-btn
+                        stretch
+                        flat
+                        label="Partners"
+                        to="/partner"
+                        no-caps
+                        size="md"
+                        class="q-mx-sm my-font-montserrat-light"
+                    /></q-item-label>
+                  </q-item-section>
+                </q-item>
+                <q-item clickable v-close-popup>
+                  <q-item-section>
+                    <q-item-label
+                      ><q-btn
+                        stretch
+                        flat
+                        label="News"
+                        to="/news"
+                        no-caps
+                        size="md"
+                        class="q-mx-sm my-font-montserrat-light"
+                    /></q-item-label>
+                  </q-item-section>
+                </q-item>
+                <q-item clickable v-close-popup>
+                  <q-item-section>
+                    <q-item-label
+                      ><q-btn
+                        stretch
+                        flat
+                        label="Contact Us"
+                        to="/contact"
+                        no-caps
+                        size="md"
+                        class="q-mx-sm my-font-montserrat-light"
+                    /></q-item-label>
+                  </q-item-section>
+                </q-item>
+              </q-list>
+            </q-menu>
+          </q-btn>
+        </div>
       </q-toolbar>
     </q-header>
-
-    <q-drawer
-      v-model="leftDrawerOpen"
-      show-if-above
-      bordered
-      content-class="bg-grey-1"
-    >
-      <q-list>
-        <q-item-label
-          header
-          class="text-grey-8"
-        >
-          Essential Links
-        </q-item-label>
-        <EssentialLink
-          v-for="link in essentialLinks"
-          :key="link.title"
-          v-bind="link"
-        />
-      </q-list>
-    </q-drawer>
 
     <q-page-container>
       <router-view />
@@ -47,61 +166,104 @@
 </template>
 
 <script>
-import EssentialLink from 'components/EssentialLink.vue'
-
-const linksData = [
-  {
-    title: 'Docs',
-    caption: 'quasar.dev',
-    icon: 'school',
-    link: 'https://quasar.dev'
-  },
-  {
-    title: 'Github',
-    caption: 'github.com/quasarframework',
-    icon: 'code',
-    link: 'https://github.com/quasarframework'
-  },
-  {
-    title: 'Discord Chat Channel',
-    caption: 'chat.quasar.dev',
-    icon: 'chat',
-    link: 'https://chat.quasar.dev'
-  },
-  {
-    title: 'Forum',
-    caption: 'forum.quasar.dev',
-    icon: 'record_voice_over',
-    link: 'https://forum.quasar.dev'
-  },
-  {
-    title: 'Twitter',
-    caption: '@quasarframework',
-    icon: 'rss_feed',
-    link: 'https://twitter.quasar.dev'
-  },
-  {
-    title: 'Facebook',
-    caption: '@QuasarFramework',
-    icon: 'public',
-    link: 'https://facebook.quasar.dev'
-  },
-  {
-    title: 'Quasar Awesome',
-    caption: 'Community Quasar projects',
-    icon: 'favorite',
-    link: 'https://awesome.quasar.dev'
-  }
-];
-
 export default {
-  name: 'MainLayout',
-  components: { EssentialLink },
-  data () {
+  name: "MainLayout",
+  data() {
     return {
       leftDrawerOpen: false,
-      essentialLinks: linksData
-    }
-  }
-}
+    };
+  },
+};
 </script>
+
+<style lang="scss">
+@font-face {
+  font-family: montserrat-black;
+  src: url("/fonts/montserrat/Montserrat-Black.ttf");
+}
+
+@font-face {
+  font-family: montserrat-extraBold;
+  src: url("/fonts/montserrat/Montserrat-ExtraBold.ttf");
+}
+
+@font-face {
+  font-family: montserrat-bold;
+  src: url("/fonts/montserrat/Montserrat-Bold.ttf");
+}
+
+@font-face {
+  font-family: montserrat-regular;
+  src: url("/fonts/montserrat/Montserrat-Regular.ttf");
+}
+
+@font-face {
+  font-family: montserrat-italic;
+  src: url("/fonts/montserrat/Montserrat-Italic.ttf");
+}
+
+@font-face {
+  font-family: montserrat-boldItalic;
+  src: url("/fonts/montserrat/Montserrat-BoldItalic.ttf");
+}
+
+@font-face {
+  font-family: montserrat-light;
+  src: url("/fonts/montserrat/Montserrat-Light.ttf");
+}
+
+@font-face {
+  font-family: montserrat-extraLight;
+  src: url("/fonts/montserrat/Montserrat-ExtraLight.ttf");
+}
+
+@font-face {
+  font-family: montserrat-thin;
+  src: url("/fonts/montserrat/Montserrat-Thin.ttf");
+}
+
+@font-face {
+  font-family: greatvibes-regular;
+  src: url("/fonts/greatvibes/GreatVibes-Regular.ttf");
+}
+
+.my-font-montserrat-black {
+  font-family: "montserrat-black";
+}
+
+.my-font-montserrat-extraBold {
+  font-family: "montserrat-extraBold";
+}
+
+.my-font-montserrat-bold {
+  font-family: "montserrat-bold";
+}
+
+.my-font-montserrat-regular {
+  font-family: "montserrat-regular";
+}
+
+.my-font-montserrat-italic {
+  font-family: "montserrat-italic";
+}
+
+.my-font-montserrat-boldItalic {
+  font-family: "montserrat-boldItalic";
+}
+
+.my-font-montserrat-light {
+  font-family: "montserrat-light";
+}
+
+.my-font-montserrat-extraLight {
+  font-family: "montserrat-extraLight";
+}
+
+.my-font-montserrat-thin {
+  font-family: "montserrat-thin";
+}
+
+.my-font-greatvibes-regular {
+  font-family: "greatvibes-regular";
+}
+</style>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,14 +1,181 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-full.svg"
-    >
+  <q-page>
+    <header>
+      <q-carousel
+        style="height: calc(100vh - 48px)"
+        animated
+        v-model="slide"
+        arrows
+        control-color="white"
+        navigation-icon="radio_button_unchecked"
+        navigation
+        infinite
+      >
+        <q-carousel-slide :name="1" img-src="/images/Dr. Manzo Maigari3.jpg">
+          <div
+            style="height: 80vh"
+            class="flex column justify-center items-start q-px-xl"
+          >
+            <span
+              class="text-white text-h4 my-font-montserrat-bold text-uppercase q-pa-0"
+            >
+              Dr. Manzo Maigari
+            </span>
+            <span
+              class="text-pink text-h4 my-font-montserrat-bold text-uppercase q-pa-0"
+            >
+              Purpose. Passion.
+            </span>
+            <span
+              class="text-pink text-h4 my-font-montserrat-bold text-uppercase q-pa-0"
+            >
+              Mission. Values.
+            </span>
+            <span class="text-white text-body1 my-font-montserrat-light"
+              >Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+              Dolorum, magni.</span
+            >
+            <span class="text-white text-body1 my-font-montserrat-light"
+              >Non voluptates voluptate dolorem laborum inventore excepturi
+              exercitationem?</span
+            >
+            <a
+              href="#"
+              class="custom__btn-pink text-pink text-uppercase my-font-montserrat-regular q-mt-md"
+              >Get Started</a
+            >
+          </div>
+        </q-carousel-slide>
+        <q-carousel-slide :name="2" img-src="/images/Dr. Manzo Maigari1.jpg">
+          <div
+            style="height: 80vh"
+            class="flex column justify-center items-start q-px-xl"
+          >
+            <span
+              class="text-white text-h4 my-font-montserrat-bold text-uppercase q-pa-0"
+            >
+              Dr. Manzo Maigari
+            </span>
+            <span
+              class="text-pink text-h4 my-font-montserrat-bold text-uppercase q-pa-0"
+            >
+              Purpose. Passion.
+            </span>
+            <span
+              class="text-pink text-h4 my-font-montserrat-bold text-uppercase q-pa-0"
+            >
+              Mission. Values.
+            </span>
+            <span class="text-white text-body1 my-font-montserrat-light"
+              >Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+              Dolorum, magni.</span
+            >
+            <span class="text-white text-body1 my-font-montserrat-light"
+              >Non voluptates voluptate dolorem laborum inventore excepturi
+              exercitationem?</span
+            >
+            <a
+              href="#"
+              class="custom__btn-pink text-pink text-uppercase my-font-montserrat-regular q-mt-md"
+              >Get Started</a
+            >
+          </div>
+        </q-carousel-slide>
+      </q-carousel>
+    </header>
+    <!-- <div style="height: 1000px">
+      <img alt="Quasar logo" src="~assets/quasar-logo-full.svg" />
+    </div> -->
+    <div style="height: 1000px">Yo</div>
   </q-page>
 </template>
 
 <script>
 export default {
-  name: 'PageIndex'
-}
+  name: "PageIndex",
+  data: () => ({
+    slide: 1,
+  }),
+};
 </script>
+
+<style lang="scss">
+.custom__btn-pink {
+  text-decoration: none;
+  padding: 15px 50px;
+  border: 2px solid #ea4b64;
+}
+
+.custom__btn-white {
+  text-decoration: none;
+  padding: 15px 50px;
+  border: 2px solid white;
+}
+
+.btn {
+  font-size: 1.5rem;
+  font-weight: 300;
+  text-transform: uppercase;
+  border-radius: none;
+  border: none;
+  background-image: -webkit-gradient(
+    linear,
+    left top,
+    right top,
+    from(#ea4b64),
+    to(#c33f5b)
+  );
+  background-image: linear-gradient(to right, #ea4b64, #c33f5b);
+  color: #fff;
+  position: relative;
+  overflow: hidden;
+  cursor: pointer;
+}
+.btn > * {
+  display: inline-block;
+  height: 100%;
+  width: 100%;
+  -webkit-transition: all 0.2s;
+  transition: all 0.2s;
+}
+.btn__visible {
+  padding: 2rem 7.5rem;
+}
+@media only screen and (max-width: 37.5em) {
+  .btn__visible {
+    padding: 1rem 3.5rem;
+  }
+}
+.btn__invisible {
+  position: absolute;
+  padding: 2rem 0;
+  left: 0;
+  top: -100%;
+}
+.btn:hover {
+  background-image: -webkit-gradient(
+    linear,
+    right top,
+    left top,
+    from(var(--color-primary-light)),
+    to(var(--color-primary-dark))
+  );
+  background-image: linear-gradient(
+    to left,
+    var(--color-primary-light),
+    var(--color-primary-dark)
+  );
+}
+.btn:hover .btn__visible {
+  -webkit-transform: translateY(100%);
+  transform: translateY(100%);
+}
+.btn:hover .btn__invisible {
+  top: 0;
+}
+.btn:focus {
+  outline: none;
+  -webkit-animation: pulsate 1s infinite;
+  animation: pulsate 1s infinite;
+}
+</style>
